90e6de6b644efeff6282df42df7d3c0e6fa6c484
FC <i4never@163.com>
2016-05-14 00:29:38 +0800

    add linea interplotation

--- datafield.cpp
+++ datafield.cpp
@@ -12,6 +12,7 @@ DataField::DataField()
     slices = NULL;
     num = 0;
     pixel_space = 0;
+    z_step = 0;
 }
 
 DataField::~DataField()
@@ -83,6 +84,9 @@ bool DataField::Adjust()
     //z
     limit[0][2] = slices[0]->coordinate[2];
     limit[1][2] = slices[num-1]->coordinate[2];
+    
+    //z-step
+    z_step = slices[1]->coordinate[2] - slices[0]->coordinate[2];
     return true;
 }
 
@@ -94,7 +98,8 @@ void DataField::Show()
         cout<<"Pictrure "<<num<<"   height:"<<slices[i]->mat->r<<"   width:"<<slices[i]->mat->c<<"   coordinate:";
         cout<<slices[i]->coordinate[0]<<"   "<<slices[i]->coordinate[1]<<"   "<<slices[i]->coordinate[2]<<"   "<<endl;
     }
-    cout<<"pixel spacing is:"<<pixel_space<<endl;
+    cout<<"pixel spacing is: "<<pixel_space<<endl;
+    cout<<"z step is: "<<z_step<<endl;
     cout<<"x limit is from "<<limit[0][0]<<" to "<<limit[1][0]<<endl;
     cout<<"y limit is from "<<limit[0][1]<<" to "<<limit[1][1]<<endl;
     cout<<"z limit is from "<<limit[0][2]<<" to "<<limit[1][2]<<endl;
@@ -109,4 +114,55 @@ bool DataField::Is_InField(Matrix coor)
     if (coor.elmt[0][2] < limit[0][2] || coor.elmt[0][2] > limit[1][2])
         return false;
     return true;
+}
+
+double DataField::GetValue(Matrix coor)
+{
+    double result;
+    double value[8];
+    if (!Is_InField(coor))
+        return -1.0;
+    
+    // from up right font, opposite clock direction
+    //find first coordinate
+    int x,y,z;  //index, not coordinate
+    x = int((coor.elmt[0][0] - limit[0][0])/pixel_space) + 1;
+    y = int((coor.elmt[0][1] - limit[0][1])/pixel_space) + 1;
+    z = int((coor.elmt[0][2]-limit[0][2])/z_step) + 1;
+    
+//    cout<<"***** "<<x<<"    "<<y<<" "<<z<<endl;
+    value[0] = slices[z]->mat->elmt[x][y];
+    value[1] = slices[z]->mat->elmt[x-1][y];
+    value[2] = slices[z]->mat->elmt[x-1][y-1];
+    value[3] = slices[z]->mat->elmt[x][y-1];
+    value[4] = slices[z-1]->mat->elmt[x][y];
+    value[5] = slices[z-1]->mat->elmt[x-1][y];
+    value[6] = slices[z-1]->mat->elmt[x-1][y-1];
+    value[7] = slices[z-1]->mat->elmt[x][y-1];
+    
+    for (int i = 0 ; i < 8 ; i++)
+        cout<<value[i]<<"   ";
+    cout<<endl;
+
+    double prop_x, prop_y, prop_z;
+    prop_x = (limit[0][0]+x*pixel_space-coor.elmt[0][0])/pixel_space;
+    prop_y = (limit[0][1]+y*pixel_space-coor.elmt[0][1])/pixel_space;
+    prop_z = (limit[0][2]+z*z_step-coor.elmt[0][2])/z_step;
+//    cout<<"propotion: "<<prop_x<<"  "<<prop_y<<"    "<<prop_z<<endl;
+    
+    double v12,v03,v47,v56,vb,vf;
+    v12 = value[1]*(1-prop_y)+value[2]*prop_y;
+    v03 = value[0]*(1-prop_y)+value[3]*prop_y;
+    v47 = value[4]*(1-prop_y)+value[7]*prop_y;
+    v56 = value[5]*(1-prop_y)+value[6]*prop_y;
+    
+    vb = v12*(1-prop_z)+v56*prop_z;
+    vf = v03*(1-prop_z)+v47*prop_z;
+    
+    result = vf*(1-prop_x)+vb*prop_x;
+
+    cout<<"result:"<<result<<endl;
+    
+    
+    return result;
 }
--- datafield.h
+++ datafield.h
@@ -6,6 +6,7 @@ public:
     Slice ** slices;
     int num;
     double pixel_space;
+    double z_step;
     double limit[2][3];
 public:
     DataField ();                   //init
@@ -14,4 +15,5 @@ public:
     bool Adjust();                  //sort accroding t z-index move z-index to make the datafiled's center location is near (0,0,0)
     bool Is_InField(Matrix coor);   //judge while a coordinate is in the datafield
     void Show();
+    double GetValue(Matrix coor);   //interplotion
 };
